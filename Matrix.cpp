35b2a04a19d9203dc1c5efc51221edbaa4b14cab
SevenMoor <au.ottaviano@yahoo.com>
2021-02-13 01:26:34 +0100

    Fix: deep copy, initialization, multiplication

--- Matrix.cpp
+++ Matrix.cpp
@@ -4,34 +4,32 @@
 #include <cblas.h>
 
 Matrix::~Matrix(){
-    free(data);
+    //free(data);
 }
 
-Matrix::Matrix(int rows, int columns, double** data) : rows(rows), columns(columns)
-{
-    this->data = (double**) malloc(rows*columns*sizeof(double));
+Matrix::Matrix(int rows, int columns, double* data) : rows(rows), columns(columns){
+    this->data = (double*) malloc(rows*columns*sizeof(double));
     for(int i=0;i<rows;i++)
         for(int j=0;j<columns;j++)
-            this->data[i][j] = data[i][j];
+            this->data[columns*i+j] = data[columns*i+j];
 }
 
-Matrix::Matrix(int rows, int columns, int archetype = MatrixArchetype::ZEROS) : rows(rows), columns(columns)
-{
+Matrix::Matrix(int rows, int columns, char archetype = MatrixArchetype::ZEROS) : rows(rows), columns(columns){
     elements = rows*columns;
-    data = (double**) malloc(rows*columns*sizeof(double));
+    data = (double*) malloc(rows*columns*sizeof(double));
     switch(archetype){
         case MatrixArchetype::ZEROS:
             for(int i=0;i<rows;i++)
                 for(int j=0;j<columns;j++)
-                    data[i][j] = 0.0;
+                    data[columns*i+j] = 0.0;
         case MatrixArchetype::ONES:
             for(int i=0;i<rows;i++)
                 for(int j=0;j<columns;j++)
-                    data[i][j] = 0.0;
+                    data[columns*i+j] = 0.0;
         case MatrixArchetype::ID:
             for(int i=0;i<rows;i++)
                 for(int j=0;j<columns;j++)
-                    data[i][j] = (i==j)? 1.0 : 0.0;
+                    data[columns*i+j] = (i==j)? 1.0 : 0.0;
     }
 }
 
@@ -39,39 +37,48 @@ Matrix::Matrix(const Matrix& m){
     rows = m.rows;
     columns = m.columns;
     elements = rows*columns;
-    this->data = (double**) malloc(rows*columns*sizeof(double));
+    this->data = (double*) malloc(rows*columns*sizeof(double));
     for (int i=0;i<rows;i++)
         for(int j=0;j<columns;j++)
-            data[i][j] = m.data[i][j];
+            data[columns*i+j] = m.data[m.columns*i+j];
 }
 
 //Check for column bounds
 void Matrix::putColumn(int j, double* elems){
     for(int i=0;i<rows;i++)
-        data[i][j] = elems[i];
+        data[columns*i+j] = elems[i];
 }
 
 //Check for row bounds
 void Matrix::putRow(int i, double* elems){
     for(int j=0;j<columns;j++)
-        data[i][j] = elems[j];
+        data[columns*i+j] = elems[j];
 }
 
 //Check for column and line bounds
 void Matrix::put(int i, int j, double elem){
-    data[i][j] = elem;
+    data[columns*i+j] = elem;
+}
+
+//Check dimensions
+void Matrix::putSubmatrix(int startRow, int startColumn, Matrix const& m){
+    for(int i=startRow;i<m.rows+startRow;i++){
+        for(int j=startColumn;j<m.columns+startColumn;j++){
+            data[columns*i+j] = m.data[m.columns*i+j];
+        }
+    }
 }
 
 //Check for indexes bounds
 double Matrix::get(int i, int j){
-    return data[i][j];
+    return data[columns*i+j];
 }
 
 //Check for row bound
 double* Matrix::getRow(int i){
     double* res = (double*) malloc(columns*sizeof(double));
     for(int j=0;j<columns;j++)
-        res[j] = data[i][j];
+        res[j] = data[columns*i+j];
     return res;
 }
 
@@ -79,11 +86,11 @@ double* Matrix::getRow(int i){
 double* Matrix::getColumn(int j){
     double* res = (double*) malloc(rows*sizeof(double));
     for(int i=0;i<rows;i++)
-        res[i] = data[i][j];
+        res[i] = data[columns*i+j];
     return res;
 }
 
-double** Matrix::getData(){
+double* Matrix::getData(){
     return data;
 }
 
@@ -105,23 +112,32 @@ int Matrix::getElements(){
 //Check for columns==other->rows
 Matrix& Matrix::operator*=(Matrix const& other){
     Matrix c(this->rows,other.columns,MatrixArchetype::ZEROS);
-    cblas_dgemm(CblasRowMajor,CblasNoTrans,CblasNoTrans,this->rows,other.columns,this->rows,1.0,*data,this->rows,*(other.data),other.columns,0.0,*(c.getData()),this->rows);
-    return c;
+    double* c_tab = (double*) malloc(c.getRows()*c.getColumns()*sizeof(double));
+    cblas_dgemm(CblasRowMajor,CblasNoTrans,CblasNoTrans,this->rows,other.columns,this->columns,1.0,data,this->rows,other.data,other.columns,0.0,c_tab,other.columns);
+    c.setData(c_tab);
+    *this = c;
+    return *this;
+}
+
+void Matrix::setData(double* data){
+    for(int i=0;i<rows;i++)
+        for(int j=0;j<columns;j++)
+            this->data[columns*i+j] = data[columns*i+j];
 }
 
 //Check for equal sizes
 Matrix& Matrix::operator+=(Matrix const& other){
     for (int i=0;i<rows;i++)
-        for(int j=0;i<columns;j++)
-            data[i][j] += other.data[i][j];
+        for(int j=0;j<columns;j++)
+            data[columns*i+j] += other.data[other.columns*i+j];
     return *this;
 }
 
 //Check for equal sizes
 Matrix& Matrix::operator-=(Matrix const& other){
     for (int i=0;i<rows;i++)
-        for(int j=0;i<columns;j++)
-            data[i][j] -= other.data[i][j];
+        for(int j=0;j<columns;j++)
+            data[columns*i+j] -= other.data[other.columns*i+j];
     return *this;
 }
 
@@ -147,10 +163,25 @@ bool Matrix::operator==(Matrix const& other){
     if(columns!=other.columns||rows!=other.rows) return false;
     for(int i=0;i<rows;i++)
         for(int j=0;j<columns;j++)
-            if(data[i][j]!=other.data[i][j]) return false;
+            if(data[columns*i+j]!=other.data[other.columns*i+j]) return false;
     return true;
 }
 
 bool Matrix::operator!=(Matrix const& other){
     return (*this==other)? false : true;
+}
+
+std::ostream& operator<<(std::ostream& os, const Matrix& m)
+{
+    for(int i=0;i<m.rows;i++){
+        os << "[ ";
+        for(int j=0;j<m.columns;j++){
+            char buff[100];
+            snprintf(buff, sizeof(buff), "%10.2lf ", m.data[m.columns*i+j]);
+            std::string formatted = buff;
+            os << formatted << " ";
+        }
+        os << "]" << std::endl;
+    }
+    return os;
 }
--- Matrix.h
+++ Matrix.h
@@ -1,12 +1,13 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
-namespace MatrixArchetype{
-    static const int ZEROS = 0;
-    static const int ONES = 1;
-    static const int ID = 2;
-}
+#include <iostream>
 
+namespace MatrixArchetype{
+    static const char ZEROS = '0';
+    static const char ONES = '1';
+    static const char ID = 'I';
+}
 
 class Matrix{
     private:
@@ -14,27 +15,30 @@ class Matrix{
         int columns;
         int elements;
         int type;
-        double** data;
+        double* data;
 
     public:
-        Matrix(int rows, int columns, double** data);
-        Matrix(int rows, int columns, int archetype);
+        explicit Matrix(int rows, int columns, double* data);
+        Matrix(int rows, int columns, char archetype);
         Matrix(const Matrix& m);
         ~Matrix();
 
         void putColumn(int j, double* elems);
         void putRow(int i, double* elems);
         void put(int i, int j, double elem);
+        void putSubmatrix(int startRow, int startColumn, Matrix const& m);
 
         double get(int i, int j);
         double* getRow(int i);
         double* getColumn(int j);
 
-        double** getData();
+        double* getData();
         int getRows();
         int getColumns();
         int getType();
         int getElements(); 
+
+        void setData(double* data);
 
         Matrix operator*(Matrix const& other);
         Matrix operator+(Matrix const& other);
@@ -44,6 +48,8 @@ class Matrix{
         Matrix& operator-=(Matrix const& other);
         bool operator==(Matrix const& other);
         bool operator!=(Matrix const& other);
+
+        friend std::ostream& operator<<(std::ostream& os, const Matrix& m);
 };
 
 
